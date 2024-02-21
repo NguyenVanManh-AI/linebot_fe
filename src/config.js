b275b18d1750b0b10bf77294d152a87b943dc744
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-02-21 22:59:49 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default{
 	// API_URL :'http://localhost:3000' , //API_URL 
-	// API_URL : 'http://192.168.1.120:8000/api',
-	API_URL : 'http://127.0.0.1:8000/api/',
-	// URL : 'http://192.168.1.120:8000/'
-	URL : 'http://127.0.0.1:8000/'
+	API_URL : 'http://192.168.1.120:8000/api/',
+	// API_URL : 'http://127.0.0.1:8000/api',
+	URL : 'http://192.168.1.120:8000/'
+	// URL : 'http://127.0.0.1:8000/'
 	// API_URL : 'http://nguyenvanmanh.lovestoblog.com'
 }
